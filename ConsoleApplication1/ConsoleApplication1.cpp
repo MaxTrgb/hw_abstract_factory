03668f38825d7be9b8b33b28916ddd430ed907dd
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2023-10-31 11:41:16 +0200

    final

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,96 +1,106 @@
 #include <iostream>
 using namespace std;
 
+// Define the abstract base classes for Hero and Enemy.
 class Hero {
 public:
-    virtual void attack() = 0;
+	virtual void attack() = 0;
+	virtual ~Hero() {} // Define a virtual destructor for proper cleanup.
 };
 
 class Enemy {
 public:
-    virtual void attack() = 0;
+	virtual void attack() = 0;
+	virtual ~Enemy() {} // Define a virtual destructor for proper cleanup.
 };
 
+// Create specific hero and enemy classes.
 class Knight : public Hero {
 public:
-    void attack() override {
-        cout << "Knight attacks with a sword." << endl;
-    }
+	void attack() override {
+		cout << "Knight attacks with a sword." << endl;
+	}
 };
 
 class Archer : public Hero {
 public:
-    void attack() override {
-        cout << "Archer attacks with a bow and arrows." << endl;
-    }
+	void attack() override {
+		cout << "Archer attacks with a bow and arrows." << endl;
+	}
 };
 
 class Goblin : public Enemy {
 public:
-    void attack() override {
-        cout << "Goblin attacks with a club." << endl;
-    }
+	void attack() override {
+		cout << "Goblin attacks with a club." << endl;
+	}
 };
 
 class Witch : public Enemy {
 public:
-    void attack() override {
-        cout << "Witch attacks with magic spells." << endl;
-    }
+	void attack() override {
+		cout << "Witch attacks with magic spells." << endl;
+	}
 };
 
+// Define the abstract factory interface for creating heroes and enemies.
 class GameFactory {
 public:
-    virtual Hero* createHero() = 0;
-    virtual Enemy* createEnemy() = 0;
+	virtual Hero* createHero() = 0;
+	virtual Enemy* createEnemy() = 0;
+	virtual ~GameFactory() {} // Define a virtual destructor for proper cleanup.
 };
 
+// Implement specific factories for the fantasy and science fiction worlds.
 class FantasyGameFactory : public GameFactory {
 public:
-    Hero* createHero() override {
-        return new Knight();
-    }
+	Hero* createHero() override {
+		return new Knight();
+	}
 
-    Enemy* createEnemy() override {
-        return new Goblin();
-    }
+	Enemy* createEnemy() override {
+		return new Goblin();
+	}
 };
 
 class SciFiGameFactory : public GameFactory {
 public:
-    Hero* createHero() override {
+	Hero* createHero() override {
+		return new Archer();
+	}
 
-        return new Archer();
-    }
-
-    Enemy* createEnemy() override {
-        return new Witch();
-    }
+	Enemy* createEnemy() override {
+		return new Witch();
+	}
 };
 
+// Function to test the factory and create heroes and enemies.
+void testFactory(GameFactory* factory) {
+	Hero* hero = factory->createHero();
+	Enemy* enemy = factory->createEnemy();
+
+	cout << "Heroes and Enemies:" << endl;
+	hero->attack();
+	enemy->attack();
+
+	delete hero;  // Clean up the created hero object.
+	delete enemy; // Clean up the created enemy object.
+}
+
 int main() {
-    GameFactory* fantasyFactory = new FantasyGameFactory();
+	// Create instances of specific factories for fantasy and science fiction worlds.
+	GameFactory* fantasyFactory = new FantasyGameFactory();
+	GameFactory* sciFiFactory = new SciFiGameFactory();
 
-    Hero* fantasyHero = fantasyFactory->createHero();
-    Enemy* fantasyEnemy = fantasyFactory->createEnemy();
+	cout << "Fantasy World:" << endl;
+	testFactory(fantasyFactory); // Test and create heroes and enemies in the fantasy world.
 
-    cout << "Fantasy World:" << endl;
-    fantasyHero->attack();
-    fantasyEnemy->attack();
+	cout << "\nScience Fiction World:" << endl;
+	testFactory(sciFiFactory); // Test and create heroes and enemies in the science fiction world.
 
-    GameFactory* sciFiFactory = new SciFiGameFactory();
+	// Clean up the factory instances.
+	delete fantasyFactory;
+	delete sciFiFactory;
 
-    Hero* sciFiHero = sciFiFactory->createHero();
-    Enemy* sciFiEnemy = sciFiFactory->createEnemy();
-
-    cout << "\nScience Fiction World:" << endl;
-    sciFiHero->attack();
-    sciFiEnemy->attack();
-
-    delete fantasyHero;
-    delete fantasyEnemy;
-    delete sciFiHero;
-    delete sciFiEnemy;
-
-    return 0;
+	return 0;
 }
